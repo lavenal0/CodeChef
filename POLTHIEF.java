7ad2d61b93f49f59a848e70b003a00d055c45b82
Abhishek Singh <singhabhishek2815@gmail.com>
2022-09-19 21:22:50 +0530

    Add files via upload

---
+++ POLTHIEF.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner obj=new Scanner(System.in);
+		int T=obj.nextInt();
+		while(T-->0){
+		    int x=obj.nextInt();
+		    int y=obj.nextInt();
+		    x-=y;
+		    if(x<0){
+		        System.out.println(x*(-1));
+		    }else{
+		        System.out.println(x);
+		    }
+		}
+	}
+}

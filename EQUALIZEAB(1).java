8cd407452517e3aeef826007cb5d91d424c48faa
Abhishek Singh <singhabhishek2815@gmail.com>
2022-09-26 21:57:14 +0530

    Add files via upload

---
+++ EQUALIZEAB(1).java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner obj=new Scanner(System.in);
+		int T=obj.nextInt();
+		int s;
+		for(int i=0;i<T;i++){
+		    int X1=obj.nextInt();
+		    int X2=obj.nextInt();
+		    int N=obj.nextInt();
+		    if(X1<X2){
+		        s=X2-X1;
+		    }
+		    else{
+		        s=X1-X2;
+		    }
+		    if ( s%(N*2) ==0 || X1==X2 ) {
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("NO");
+            }
+		    
+		}
+	}
+}
